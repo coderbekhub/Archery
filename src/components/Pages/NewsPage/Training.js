ab8404c08b42064115455af6bc83a7539f60fbf5
MirzohidovMirsodiq <coderbek@mail.ru>
2022-12-07 11:55:57 +0500

    all page

--- src/components/Pages/NewsPage/Training.js
+++ src/components/Pages/NewsPage/Training.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import GoogleMaps from '../../googleMap/GoogleMaps'
+import '../../../styles/NewsPage.css'
+import TrainingImg from '../../../img/training_img.jpg'
 
 export default function Training() {
   return (
     <>
-    <h1>Training</h1>
+      <div className="container">
+        <div className="training_content">
+          <h2>Учебно-тренировичные сборы</h2>
+          <div className="training_info">
+            <div className='training_img_content'>
+              <img src={TrainingImg} alt="" />
+            </div>
+              <span className='training_photo_date'>the time the information was posted</span>
+          </div>
+        </div>
+      </div>
       <GoogleMaps />
     </>
   )
