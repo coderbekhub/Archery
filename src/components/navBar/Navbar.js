755a413a67832bff26de1f854c985023811f1266
MirzohidovMirsodiq <coderbek@mail.ru>
2022-12-12 18:05:49 +0500

    faqat news page mediasi qoldi

--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -38,10 +38,10 @@ export default function Navbar(props) {
             </button> */}
             
             <ul className="nav_list">
-              <li><Link to="/" className="nav_link">Главная</Link></li>
-              <li><Link to="/federation" className="nav_link">НАША ФЕДЕРАЦИЯ</Link></li>
-              <li><Link to="/gallery" className="nav_link">ГАЛЕРЕЯ</Link></li>
-              <li><Link to="/competition" className="nav_link">СОРЕВНОВАНИЯ</Link></li>
+              <li><Link to="/" className="nav_link" onClick={showNavbar}>Главная</Link></li>
+              <li><Link to="/federation" className="nav_link" onClick={showNavbar}>НАША ФЕДЕРАЦИЯ</Link></li>
+              <li><Link to="/gallery" className="nav_link" onClick={showNavbar}>ГАЛЕРЕЯ</Link></li>
+              <li><Link to="/competition" className="nav_link" onClick={showNavbar}>СОРЕВНОВАНИЯ</Link></li>
               {/* <li>
               <Link className="nav_link">
                 <Dropdown isOpen={dropdownOpen} toggle={toggle} {...props}>
@@ -79,16 +79,16 @@ export default function Navbar(props) {
                     НОВОСТИ
                   </DropdownToggle>
                   <DropdownMenu>
-                    <DropdownItem><Link to='/intervu' className='records'>Интервью</Link></DropdownItem>
+                    <DropdownItem><Link to='/intervu' className='records' onClick={showNavbar}>Интервью</Link></DropdownItem>
                     {/* <DropdownItem><Link to='/photo' className='records'>Фото</Link></DropdownItem> */}
-                    <DropdownItem><Link to='/video'  className='records'>Видео</Link></DropdownItem>
-                    <DropdownItem><Link to='/training' className='records'>Учебно-тренировичные сборы</Link></DropdownItem>
+                    <DropdownItem><Link to='/video'  className='records' onClick={showNavbar}>Видео</Link></DropdownItem>
+                    <DropdownItem><Link to='/training' className='records' onClick={showNavbar}>Учебно-тренировичные сборы</Link></DropdownItem>
                   </DropdownMenu>
                 </Dropdown>
                 </Link>
               </li>
 
-              <li><Link to="/contacts" className="nav_link">КОНТАКТЫ</Link></li>
+              <li><Link to="/contacts" className="nav_link" onClick={showNavbar}>КОНТАКТЫ</Link></li>
             </ul>
           </nav>
         </div>
