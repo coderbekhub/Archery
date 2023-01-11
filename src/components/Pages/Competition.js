f29d69fd2ba25abdeaa6895097159e6ae0903445
MirzohidovMirsodiq <coderbek@mail.ru>
2023-01-11 11:00:41 +0500

    all language translated

--- src/components/Pages/Competition.js
+++ src/components/Pages/Competition.js
@@ -2,46 +2,66 @@ import React from 'react'
 import GoogleMaps from '../googleMap/GoogleMaps'
 import Kubok from '../../img/kubok.png'
 import '../../styles/Competition.css'
+import { useTranslation } from 'react-i18next';
 
 export default function Competition() {
+  const {t} = useTranslation()
+  const language = [
+    {
+      code: 'uz',
+      name: 'O‘zbek',
+      country_code: 'uz'
+    },
+    {
+      code: 'ru',
+      name: 'Русский',
+      country_code: 'ru'
+    },
+    {
+      code: 'en',
+      name: 'English',
+      country_code: 'gb'
+    }
+  ]
+
   return (
     <>
       <div className="container">
         <div className='competition_flex'>
           <div className='competition_content'>
-            <h3>20 <span className='competition_month'>May</span></h3>
+            <h3>20 <span className='competition_month'>{t('competitionMonth')}</span></h3>
             <img src={Kubok} alt="Kubok" />
             <div className='competition_info'>
               <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum!</h2>
               <span>19-05-2022 - 22-05-2022</span>
-              <span>Tashkent</span>
+              <span>{t('competitionCity')}</span>
             </div>
           </div>
           <div className='competition_content'>
-            <h3>20 <span className='competition_month'>May</span></h3>
+            <h3>20 <span className='competition_month'>{t('competitionMonth')}</span></h3>
             <img src={Kubok} alt="Kubok" />
             <div className='competition_info'>
               <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum!</h2>
               <span>19-05-2022 - 22-05-2022</span>
-              <span>Tashkent</span>
+              <span>{t('competitionCity')}</span>
             </div>
           </div>
           <div className='competition_content'>
-            <h3>20 <span className='competition_month'>May</span></h3>
+            <h3>20 <span className='competition_month'>{t('competitionMonth')}</span></h3>
             <img src={Kubok} alt="Kubok" />
             <div className='competition_info'>
               <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum!</h2>
               <span>19-05-2022 - 22-05-2022</span>
-              <span>Tashkent</span>
+              <span>{t('competitionCity')}</span>
             </div>
           </div>
           <div className='competition_content'>
-            <h3>20 <span className='competition_month'>May</span></h3>
+            <h3>20 <span className='competition_month'>{t('competitionMonth')}</span></h3>
             <img src={Kubok} alt="Kubok" />
             <div className='competition_info'>
               <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum!</h2>
               <span>19-05-2022 - 22-05-2022</span>
-              <span>Tashkent</span>
+              <span>{t('competitionCity')}</span>
             </div>
           </div>
         </div>
--- src/components/Pages/Contacts.js
+++ src/components/Pages/Contacts.js
@@ -1,16 +1,36 @@
 import React from 'react'
 import GoogleMaps from '../googleMap/GoogleMaps'
+import { useTranslation } from 'react-i18next';
 
 export default function Contacts() {
+  const {t} = useTranslation()
+  const language = [
+    {
+      code: 'uz',
+      name: 'O‘zbek',
+      country_code: 'uz'
+    },
+    {
+      code: 'ru',
+      name: 'Русский',
+      country_code: 'ru'
+    },
+    {
+      code: 'en',
+      name: 'English',
+      country_code: 'gb'
+    }
+  ]
+
   return (
     <>
     <div className="container">
       <div className='contact_content'>
-        <h1>Контакты</h1>
-        <p>Город Ташкент, Чиланзарский район, улица Фозилтепа, дом 38</p>
-        <a href="tel:+998909796568"><span>Тел:</span> +998909796568</a> <br />
-        <a href="tel:+998930084887"><span>Тел:</span> +998930084887</a> <br />
-        <a href="#" target={'_blank'}><span>Электронная почта:</span> uaf.office@gmail.com</a>
+        <h1>{t('contactContent/h1')}</h1>
+        <p>{t('contactContent/p')}</p>
+        <a href="tel:+998909796568"><span>{t('contactContent/tell')}</span> +998909796568</a> <br />
+        <a href="tel:+998930084887"><span>{t('contactContent/tell')}</span> +998930084887</a> <br />
+        <a href="#" target={'_blank'}><span>{t('contactContent/email')}</span> uaf.office@gmail.com</a>
       </div>
     </div>
       <GoogleMaps />
